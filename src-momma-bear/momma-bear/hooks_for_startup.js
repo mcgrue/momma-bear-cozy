c274d21bdfd693f7f1ba1a30d8f3ef835143a9ea
Ben McGraw <ben.mcgraw@gmail.com>
2021-01-18 16:44:46 -0800

    Last WIP before a major refactor; keeping this repo as the 'GAME' stuff

--- src-momma-bear/momma-bear/hooks_for_startup.js
+++ src-momma-bear/momma-bear/hooks_for_startup.js
@@ -1,5 +1,35 @@
+function addStyle(styleString) {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
+}
+
 global.runBeforeGameInit = () => {
     debugger;
+
+    addStyle(`
+    .poop {
+      background-color: red;
+      border-style: double;
+      border-coloir: white;
+      
+      color: white;
+      font-size: 2em;
+      position: absolute;
+      width: 300px;
+      height: 300px;
+      top: 10px;
+      left: 10px;
+      padding: 1em;
+      font-family: sans-serif;
+      z-index: 2;
+    }
+  `);
+
+    var elem = document.createElement('div');
+    elem.className = 'poop';
+    elem.innerHTML = 'I AM A <s>POOP</s> UI OVERLAY!!!1';
+    document.body.appendChild(elem);
 }
 
 global.runAfterGameInit = () => {
